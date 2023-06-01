ebc5e381bfc680594466bbe91e41cabacaa954b7
abdullah5abid <abdullah@webilogics.com>
2023-06-01 18:40:08 +0500

    Defined Codepipeline

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -9,7 +9,12 @@ export class CdkCicdStack extends cdk.Stack {
     new CodePipeline(this, "AwesomePipeline", {
       pipelineName: "AwesomePipeline",
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('')
+        input: CodePipelineSource.gitHub('abdullah5abid/cicd-practice', 'master'),
+        commands: [
+          'npm ci',
+          'npx cdk synth',
+        ],
+        // primaryOutputDirectory: 'cdk.output'
       })
     })
   }
